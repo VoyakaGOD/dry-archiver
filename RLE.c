d8991dd8f5487d81ee2629f9c4eb7037682847fa
VoyakaGOD <ab3000645@gmail.com>
2022-12-18 16:43:12 +0300

    RLE decode implemented

--- RLE.c
+++ RLE.c
@@ -20,12 +20,13 @@ int RLE_encode(FILE *src, FILE *dst, long max_block_size)
     if(check_files(src, dst))
         return 1;
 
+    if(max_block_size < 1)
+        max_block_size = 1;
     byte_t *src_buffer = (byte_t *)calloc(max_block_size, sizeof(byte_t));
     byte_t *dst_buffer = (byte_t *)calloc(2 * max_block_size, sizeof(byte_t));
 
     while(1)
     {
-        long src_index = 0;
         long dst_index = 0;
         long block_size = fread(src_buffer, sizeof(byte_t), max_block_size, src);
         if(block_size == 0)
@@ -55,10 +56,55 @@ int RLE_encode(FILE *src, FILE *dst, long max_block_size)
         if(block_size < max_block_size)
             break;
     }
+
+    free(src_buffer);
+    free(dst_buffer);
     return 0;
 }
 
 int RLE_decode(FILE *src, FILE *dst, long max_block_size)
 {
-    return -100;
+    if(check_files(src, dst))
+        return 1;
+
+    if(max_block_size < 1)
+        max_block_size = 1;
+    max_block_size += max_block_size % 2;
+    byte_t *src_buffer = (byte_t *)calloc(max_block_size, sizeof(byte_t));
+    byte_t *dst_buffer = (byte_t *)calloc(max_block_size, sizeof(byte_t));
+
+    while(1)
+    {
+        long dst_index = 0;
+        long block_size = fread(src_buffer, sizeof(byte_t), max_block_size, src);
+        if(block_size % 2 != 0)
+        {
+            printf("Incorrect file size!");
+            return 1;
+        }
+
+        for(long i = 0; i < block_size; i++)
+        {
+            byte_t count = src_buffer[i++];
+            byte_t byte = src_buffer[i];
+            while(count > 0)
+            {
+                if(dst_index == max_block_size)
+                {
+                    fwrite(dst_buffer, sizeof(byte_t), dst_index, dst);
+                    dst_index = 0;
+                }
+                dst_buffer[dst_index++] = byte;
+                count--;
+            }
+        }
+
+        fwrite(dst_buffer, sizeof(byte_t), dst_index, dst);
+        if(block_size < max_block_size)
+            break;
+    }
+
+    free(src_buffer);
+    free(dst_buffer);
+    return 0;
 }
--- dry.c
+++ dry.c
@@ -6,4 +6,12 @@ int main()
     FILE *output = fopen("test_out.hex", "wb");
     if(RLE_encode(input, output, 1024))
         getchar();
+    fclose(input);
+    fclose(output);
+    input = fopen("test_out.hex", "rb");
+    output = fopen("test_decoded.hex", "wb");
+    if(RLE_decode(input, output, 1024))
+        getchar();
+    fclose(input);
+    fclose(output);
 }
