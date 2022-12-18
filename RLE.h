9fce936c041bf9b14d68e626a40a64f6b61763ff
VoyakaGOD <ab3000645@gmail.com>
2022-12-18 15:38:32 +0300

    initial commit

---
+++ RLE.h
@@ -0,0 +1,13 @@
+#ifndef RLE_HEADER
+#define RLE_HEADER
+
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef unsigned char byte_t;
+
+void RLE_encode(FILE *src, FILE *dst, long block_size);
+
+void RLE_decode(FILE *src, FILE *dst, long block_size);
+
+#endif //RLE_HEADER
